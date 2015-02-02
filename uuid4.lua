67b3863f5d89a0be93cf23dd26ea88a6ab79ddce
Jeff Jacobs <jeff@bluebirdsol.com>
2015-02-02 09:16:06 -1000

    Eliminated string concatenation for 15% speed improvement.

--- uuid4.lua
+++ uuid4.lua
@@ -27,12 +27,12 @@ local M = {}
 local urandom = assert(io.open("/dev/urandom", "rb"))
 
 local function newUUID()
-    local block = urandom:read(18)
-    return string.format("%02x%02x%02x%02x-%02x%02x-", block:byte(1, 7)) ..
-            string.format("%02x%02x-", (block:byte(8) / 16) + 64, block:byte(9)) ..
-            string.format("%02x%02x-", (block:byte(10) / 4) + 128, block:byte(11)) ..
-            string.format("%02x%02x%02x%02x%02x%02x", block:byte(12, 18))
+    local block = urandom:read(16)
+    return string.format("%02x%02x%02x%02x-%02x%02x-%02x%02x-%02x%02x-%02x%02x%02x%02x%02x%02x",
+        block:byte(1), block:byte(2), block:byte(3), block:byte(4), block:byte(5), block:byte(6),
+        (block:byte(7) / 16) + 64, block:byte(8), (block:byte(9) / 4) + 128, block:byte(10), block:byte(11, 16))
 end
 
 M.newUUID = newUUID
 return M
+
